865ccd62938d8ec9d67049f004902022702ef310
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-03 18:09:42 +0300

    updated eslint config. Fixed eslint errors.

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,8 +6,8 @@
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
  */
 
-function applyCustomJoin () {
-  [].__proto__.join2 = function () {
+function applyCustomJoin() {
+  [].__proto__.join2 = function() {
     // write code here
   };
 }
