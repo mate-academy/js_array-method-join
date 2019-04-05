4a418ffde70df4bdc41269100d726f8f52b3d8be
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-05 13:58:04 +0300

    removed empty line after doc, changed typeof checking to .toBeinstanceOf, moved expectation checks to new line, removed package-lock.json from .gitignore

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -3,9 +3,7 @@
 /**
  * Implement method join
  *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
  */
-
 function applyCustomJoin() {
   [].__proto__.join2 = function() {
     // write code here
